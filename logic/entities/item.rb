a9c503c3862c74adcf9a5fa49c6dc4069c29d95c
Dachrono <andres_zamorano_@hotmail.com>
2023-09-12 16:27:55 -0600

    Add can be archived method, add move to archive method

--- logic/entities/item.rb
+++ logic/entities/item.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class Item
   attr_accessor :id, :publish_date, :archived, :genre, :author, :label, :source
 
@@ -14,4 +16,18 @@ class Item
   def add_label; end
 
   def add_source; end
+
+  def move_to_archive
+    return unless can_be_archived?
+
+    @archived = true
+  end
+
+  private
+
+  def can_be_archived?
+    current_date = Date.today
+    years = current_date.year - @publish_date.year
+    years > 10
+  end
 end
