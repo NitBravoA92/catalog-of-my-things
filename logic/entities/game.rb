851e759e3fd9ec486d275349096b364f26341705
Dachrono <andres_zamorano_@hotmail.com>
2023-09-12 19:23:43 -0600

    Solve some rubocop issues

--- logic/entities/game.rb
+++ logic/entities/game.rb
@@ -1,16 +1,16 @@
 require_relative 'item'
 
 class Game < Item
-    attr_accessor :multiplayer, :last_played_at
+  attr_accessor :multiplayer, :last_played_at
 
-    def initialize (id, publish_date, multiplayer, last_played_at)
+  def initialize(id, publish_date, multiplayer, last_played_at)
     super(id, publish_date)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
-    end 
-    
-    def can_be_archived?
-        parent_method = super 
-        parent_method || @last_played_at > 2
-    end    
-end    
+  end
+
+  def can_be_archived?
+    parent_method = super
+    parent_method || @last_played_at > 2
+  end
+end
