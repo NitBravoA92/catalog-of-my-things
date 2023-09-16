efe74d0c428117de0f34afd3366409f694c4a8d1
Dachrono <andres_zamorano_@hotmail.com>
2023-09-15 20:49:13 -0600

    Create test for games and authors

---
+++ spec/author_spec.rb
@@ -0,0 +1,30 @@
+require 'date'
+require_relative '../logic/entities/author.rb'
+require_relative '../logic/entities/game.rb'
+
+describe Author do
+    before :each do
+        @author = Author.new(2001, 'Pepe', 'Pecas')
+        @games = Game.new(3001, Date.new(2000, 5, 1), true, Date.new(2000, 5, 1))
+    end
+    
+    describe '#new' do
+        it 'Should initialize class' do
+            expect(@author).to be_instance_of (Author)
+        end
+
+        it 'Should save the name of the new author' do
+            expect(@author.first_name).to eq('Pepe') 
+        end    
+    end
+    
+    describe '#add_item' do
+    context 'when a new Item is created' do
+      it 'it should be added to the @items array' do
+        @author.add_item(@games)
+        expect(@author.items.length).to eq(1)
+        expect(@author.items[0]).to eq(@games)
+        end
+      end
+    end  
+end    
